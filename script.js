6c0184e5c4a1117e4396a886c213dac69e90979c
Jake Buroker <shamrecks@gmail.com>
2023-12-01 21:54:48 -0600

    added total salary counter

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+let totalMonthly = 0 
 function handleSubmit(event) {
         event.preventDefault();
         let tableBody = document.getElementById("tableBody");
@@ -8,7 +10,8 @@ function handleSubmit(event) {
         let employeeId = document.getElementById("idInput").value;
         let jobTitle = document.getElementById("titleInput").value;
         let annualSalary = document.getElementById("annualSalaryInput").value;
-        tableBody.innerHTML +=
+        tableBody.innerHTML += 
+   
 `
        <tr>
         <th>${firstName}</th>
@@ -27,9 +30,13 @@ function handleSubmit(event) {
     document.getElementById('idInput').value = '';
     document.getElementById('titleInput').value = '';
     document.getElementById('annualSalaryInput').value = '';
+    
+        let salaryNum = Number(annualSalary)
+         totalMonthly += salaryNum
+console.log(totalMonthly)
 }
 function deleteEmployee(event) {
     event.preventDefault();
    event.target.parentElement.parentElement.remove();
   }
-  
+
